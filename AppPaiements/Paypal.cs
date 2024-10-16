778c8ecec11bf45a493b8ce1087c256d74abbc22
leonelbrice.taffouomanfo <leonelbrice.taffouomanfo@monboreal.ca>
2024-10-16 15:44:08 -0400

    ajout de Paypal

---
+++ AppPaiements/Paypal.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppPaiements
+{
+    public class Paypal : Paiement
+    {
+        public string Courriel{ get; set; }
+        public Paypal(double montant, string description, string courriel) : base(montant, description)
+        {
+            Courriel = courriel;
+        }
+
+        public override void AfficherDetails()
+        {
+            Console.WriteLine($"Paypal - Montant : {Montant}, Description : {Description}, Courriel : {Courriel}");
+        }
+    }
+}

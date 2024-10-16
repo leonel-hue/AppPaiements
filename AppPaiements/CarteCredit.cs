07cc46e58edfb262f61820c8fd30243e9557e704
leonelbrice.taffouomanfo <leonelbrice.taffouomanfo@monboreal.ca>
2024-10-16 15:31:43 -0400

    ajout de cartecredit

---
+++ AppPaiements/CarteCredit.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppPaiements
+{
+    public class CarteCredit : Paiement
+    {
+        public int NumeroCarte { get; set; }
+
+        public CarteCredit(double montant, string description, int numeroCarte): base(montant, description)
+        {
+            NumeroCarte = numeroCarte;
+        }
+
+        public override void AfficherDetails()
+        {
+            Console.WriteLine($"Carte de credit - montant : {Montant}, Description : {Description}, Numero de carte : {NumeroCarte}");
+        }
+
+
+    }
+}

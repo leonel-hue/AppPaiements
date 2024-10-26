418658be81c185a30841e35506c37716fecb9ac7
leonelbrice.taffouomanfo <leonelbrice.taffouomanfo@monboreal.ca>
2024-10-25 22:20:56 -0400

    update

--- AppPaiements/Program.cs
+++ AppPaiements/Program.cs
@@ -1,77 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-
-namespace AppConsoleLivres
+﻿namespace AppPaiements
 {
   
-    public class Livre
-    {
-        public string Titre { get; set; }
-        public string Auteur { get; set; }
-        public int AnneePublication { get; set; }
-
-        public virtual void AfficherDetails()
-        {
-            Console.WriteLine($"Titre: {Titre}, Auteur: {Auteur}, Année: {AnneePublication}");
-        }
-    }
-
-    public class Revue : Livre
-    {
-        public int Volume { get; set; }
-
-        public override void AfficherDetails()
-        {
-            Console.WriteLine($"Revue - Titre: {Titre}, Auteur: {Auteur}, Année: {AnneePublication}, Volume: {Volume}");
-        }
-    }
-
-    public class Roman : Livre
-    {
-        public string Genre { get; set; }
-
-        public override void AfficherDetails()
-        {
-            Console.WriteLine($"Roman - Titre: {Titre}, Auteur: {Auteur}, Année: {AnneePublication}, Genre: {Genre}");
-        }
-    }
-
-    class Program
+    public class Program
     {
         static void Main(string[] args)
-        {e
-            Revue revue1 = new Revue { Titre = "Science Magazine", Auteur = "John Doe", AnneePublication = 2021, Volume = 50 };
-            Revue revue2 = new Revue { Titre = "Nature", Auteur = "Jane Smith", AnneePublication = 2020, Volume = 45 };
-            Revue revue3 = new Revue { Titre = "Time", Auteur = "Emily Johnson", AnneePublication = 2022, Volume = 100 };
+        {
+            CarteCredit carte1 = new CarteCredit(100, "Achat en ligne", 123456);
+            CarteCredit carte2 = new CarteCredit(200, "Restaurant", 654321);
+            CarteCredit carte3 = new CarteCredit(50, "Essence", 987654);
 
-            revue1.AfficherDetails();
-            revue2.AfficherDetails();
-            revue3.AfficherDetails();
+            Paypal paypal1 = new Paypal(150, "Achat numérique", "leonel@gmail.com");
+            Paypal paypal2 = new Paypal(75, "Abonnement", "leonel@gmail.com");
 
-            Roman roman1 = new Roman { Titre = "Le Petit Prince", Auteur = "Antoine de Saint-Exupéry", AnneePublication = 1943, Genre = "Fiction" };
-            Roman roman2 = new Roman { Titre = "1984", Auteur = "George Orwell", AnneePublication = 1949, Genre = "Dystopie" };
-            Roman roman3 = new Roman { Titre = "To Kill a Mockingbird", Auteur = "Harper Lee", AnneePublication = 1960, Genre = "Fiction" };
+            Utilisateur utilisateur = new Utilisateur("Leonel Brice");
 
-            roman1.AfficherDetails();
-            roman2.AfficherDetails();
-            roman3.AfficherDetails();
+            utilisateur.AjouterPaiement(carte1);
+            utilisateur.AjouterPaiement(carte2);
+            utilisateur.AjouterPaiement(carte3);
+            utilisateur.AjouterPaiement(paypal1);
+            utilisateur.AjouterPaiement(paypal2);
 
-            List<Livre> livres = new List<Livre>();
-            livres.Add(revue1);
-            livres.Add(revue2);
-            livres.Add(revue3);
-            livres.Add(roman1);
-            livres.Add(roman2);
-            livres.Add(roman3);
-
-            Console.WriteLine("\nDétails des livres dans la liste:");
-            foreach (var livre in livres)
-            {
-                livre.AfficherDetails();
-            }
-
-            Console.WriteLine("Appuyez sur une touche pour continuer...");
-            Console.ReadKey();
+            utilisateur.AfficherInfo();
         }
     }
 }
